32bb080bcb364d69863e3e9fc1a90c2762799e10
Frank Thomas <FTHOMAS@calgary.ca>
2020-02-19 12:55:15 -0700

    Add project files.

---
+++ SimpleAsyncAwaitExample/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleAsyncAwaitExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting");
+
+            var worker = new Worker();
+            worker.DoWork();
+
+            while (!worker.IsComplete)
+            {
+                Console.Write(".");
+                Thread.Sleep(100);
+            }
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+
+        private class Worker
+        {
+            public bool IsComplete { get; set; }
+
+            public async void DoWork()
+            {
+                this.IsComplete = false;
+                Console.WriteLine("Doing Work");
+
+                LongOperation();
+
+                Console.WriteLine(Environment.NewLine + "Work Completed");
+
+                IsComplete = true;
+            }
+
+            private Task LongOperation()
+            {
+                return Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine("Working!");
+                    Thread.Sleep(3000);
+                    Console.WriteLine("Done working.");
+                });
+            }
+        }
+    }
+}
